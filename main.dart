b28dd1c5c58937dd8b56498a6d8c395e8b8d28ba
AqsaBano330 <aqsabano08@gmail.com>
2024-06-06 21:46:08 +0500

    Dart Data Type

---
+++ main.dart
@@ -0,0 +1,52 @@
+void main() {
+
+//String
+
+//Record
+  Record record =("1","2","3");
+  print(record);
+  List list = ["1","2","3"];
+  list.add("7");
+   list.add("9");
+  print(list);
+
+Record record1 = ("1", "2", "3");
+print(record1);
+  //Runes
+
+String value = "a";
+print(value.runes);
+String value1 = "aqsa bano is the instructor";
+print(value1.runes);
+
+//int
+int num5 = 2+2;
+//int num = 2+2.0;
+
+//double
+double num1 =  2;
+double num2 = 4.6;
+double num3 = 2+4.6;
+double num4 = 2+2;
+
+print(num1);
+print(num2);
+print(num3);
+print(num4);
+
+//num
+
+int n1 = 2;
+double n2 = 3.0;
+ num num6 = n1 + n2;
+ print(num6);
+
+ //bool
+
+ bool abc = 14>86;
+ 
+ print(abc);
+
+ bool num7 =n1 <n2;
+ print(num7);
+}
